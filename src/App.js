b9d23470fffa150a9a9502627faa595af6c97299
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-01-20 15:45:24 +0100

    fetch API Request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Button from './components/Button';
 import Meme from './components/Meme';
 import ImageSlider from './components/ImageSlider';
 import TextInput from './components/TextInput';
 
 function App() {
+  const [pickedImage, setPickedImage] = useState();
+  const [images, setImages] = useState([]);
+  // Imgflip API endpoint
+  // https://imgflip.com/api
+  const endpoint = "https://api.imgflip.com/get_memes";
+
+  // Invoke getImages function on update of pickedImage state variable
+  useEffect(()=>{
+    getImages(endpoint).then((res)=>{
+      setImages(res);
+    })
+  },[])
+
+  const getImages = async (url) => {
+    const images = await axios
+      .get(url)
+      .then((response)=>{
+        /* Array om Memes */
+        return response.data.data.memes;
+      })
+      .catch((error)=>{console.error(error.message);})
+      return images;
+  }
+
+  console.log(images);
+
   return (
     <div className="App">
       <header className="App-header">
